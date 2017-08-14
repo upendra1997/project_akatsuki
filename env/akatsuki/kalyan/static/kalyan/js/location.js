ba69f66e614ff28c4b708f10ad157284ca1a1462
upendra1997 <Upendra.upadhyay.97@gmail.com>
2017-08-15 04:41:51 +0530

    modified:   env/akatsuki/db.sqlite3 	modified:   env/akatsuki/kalyan/static/kalyan/js/location.js 	modified:   env/akatsuki/kalyan/templates/kalyan/HE/public/login.html 	modified:   env/akatsuki/templates/base.html

--- env/akatsuki/kalyan/static/kalyan/js/location.js
+++ env/akatsuki/kalyan/static/kalyan/js/location.js
@@ -17,6 +17,7 @@ function pos(){
     }
     else
     {
+        console.log(location);
         $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCPy4FDC0ZtO9Ci7vroeIxrzkll4euPYuEY", function(data){
         alert(data);
         });
