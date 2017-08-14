d3f10699544360ffc64544ebae5c79571fc0e34f
Manoj Jeswani <manoj.jeswani@mettl.com>
2017-08-15 02:07:01 +0530

    try1

--- env/akatsuki/kalyan/admin.py
+++ env/akatsuki/kalyan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Suggestions)
+admin.site.register(Complains)
 
 # Register your models here.
